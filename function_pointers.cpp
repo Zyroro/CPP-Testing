486065ac5de6be5aa7cffed56a417a9dd31fa03e
potato <baconhairchan09@gmail.com>
2026-08-16 18:19:04 +0800

    Added Function Pointers Test

---
+++ function_pointers.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+/* Function pointers are pointers that point to a function. This allows functions to be passed through as arguments. */
+
+int add(int a, int b)
+{
+	return a + b;
+}
+
+int substract(int a, int b)
+{
+	return a - b;
+}
+
+/*
+*	
+*
+*/
+
+
+int calculate(int a, int b, int(*func)(int, int))
+{
+	return func(a, b);
+}
+
+int main()
+{
+	std::cout << calculate(1, 1, add) << '\n';
+	std::cout << calculate(2, 1, substract) << '\n';
+	return 0;
+}
